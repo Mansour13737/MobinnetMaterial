925c8de608ac047ced35fda569431c92207e8232
Mansour nosrati <mansournosrati69@gmail.com>
2025-12-05 23:21:05 +0000

    Please choose better placeholder images here (_for element img_)

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -19,7 +19,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
             <Avatar className="h-10 w-10">
-                <AvatarImage src="https://i.pravatar.cc/150?u=a042581f4e29026704d" alt="مبین نت" />
+                <AvatarImage src="https://picsum.photos/seed/mobinnet/150/150" alt="مبین نت" />
                 <AvatarFallback>
                     <User />
                 </AvatarFallback>
